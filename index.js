efdbd53cce5506005b957fd90fc67efc939fc6e6
Makcy <644215038@qq.com>
2018-01-17 13:17:53 +0800

    add

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const request = require('request');
+const parse = require('./util/parse');
 
-request('https://my.hupu.com/search?fid=34&type=topic&q=%E3%80%8A%E4%B8%8D%E5%86%B7%E7%AC%91%E8%AF%9D%E3%80%8B%E7%AC%AC', function(error, response, body) {
-  console.log(body)
-});
+parse.getPeriodList().then(c => console.log(c))
---
+++ util/parse.js
@@ -0,0 +1,8 @@
+const request = require('request');
+const config = require('../config');
+const cheerio = require('cheerio');
+
+module.exports = {
+  getUnColdList: async (page = null) => {},
+  getUnColdDetail: async () => {}
+}
